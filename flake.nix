a7d93b41ae1a35fdbc65dbfd3457d77b280ecc60
Fernando Ayats <ayatsfer@gmail.com>
2022-09-23 12:28:28 +0200

    nerd fonts

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs = {
     self,
     nixpkgs,
@@ -21,7 +22,7 @@
       perSystem = {
         pkgs,
         system,
-        self',
+        config,
         ...
       }: let
         nv = (pkgs.callPackage ./generated.nix {}).iosevka;
@@ -33,7 +34,9 @@
           ];
         };
         packages = {
-          default = pkgs.napalm.buildPackage nv.src {
+          default = config.packages.base;
+
+          base = pkgs.napalm.buildPackage nv.src {
             inherit (nv) pname version;
             npmCommands = [
               "npm install"
@@ -43,51 +46,79 @@
               pkgs.ttfautohint
             ];
             postPatch = ''
-              cp ${./private-build-plans.toml} private-build-plans.toml
-            '';
-            installPhase = ''
-              mkdir -p $out/share/fonts/truetype
-              cp -avL dist/*/ttf/* $out/share/fonts/truetype
-              cp -avL "${self'.packages.nerd-fonts-src}/src/glyphs/Symbols-1000-em Nerd Font Complete.ttf" $out/share/fonts/truetype
-            '';
-          };
-
-          web = pkgs.napalm.buildPackage nv.src {
-            pname = "${nv.pname}-web";
-            inherit (nv) version;
-            npmCommands = [
-              "npm install"
-              "npm run build --no-update-notifier -- webfont::iosevka-normal >/dev/null"
-            ];
-            nativeBuildInputs = [
-              pkgs.ttfautohint
-            ];
-            postPatch = ''
-              cp ${./private-build-plans.toml} private-build-plans.toml
+              cp -v ${./private-build-plans.toml} private-build-plans.toml
             '';
             installPhase = ''
               mkdir -p $out
-              find dist -type f -name '*.woff2' -exec cp -v '{}' $out \;
-              find dist -type f -name '*.css' -exec cp -v '{}' $out \;
+              cp -avL dist/*/ttf/* $out
             '';
           };
 
-          zipfile =
-            pkgs.runCommand "iosevka-zip" {
-              src = self'.packages.default;
-              nativeBuildInputs = [
-                pkgs.zip
-              ];
-            } ''
-              WORKDIR="$PWD"
-              cd $src/share/fonts/truetype
-              zip "$WORKDIR/iosevka.zip" *
-              cp -av "$WORKDIR/iosevka.zip" $out
-            '';
+          # nerd-font-patcher = pkgs.nerd-font-patcher.overrideAttrs (old: rec {
+          #   version = "2.2.2";
+          #   src = pkgs.fetchFromGitHub {
+          #     owner = "ryanoasis";
+          #     repo = "nerd-fonts";
+          #     rev = "v${version}";
+          #     sparseCheckout = ''
+          #       font-patcher
+          #       /src/glyphs
+          #     '';
+          #     hash = "sha256-gGhbwlkQhVpWoifF9R9RiRRMHTxIfKjiMZ1liIIkx8c=";
+          #   };
+          # });
 
-          web-zipfile =
-            pkgs.runCommand "iosevka-web-zip" {
-              src = self'.packages.web;
+          base-nerd = pkgs.stdenvNoCC.mkDerivation {
+            inherit (nv) pname version;
+            src = config.packages.base;
+            nativeBuildInputs = [
+              pkgs.nerd-font-patcher
+            ];
+            buildPhase = ''
+              shopt nullglob
+              set +x
+              trap 'set +x' ERR
+
+              mkdir -p $out
+              for file in ./*; do
+                nerd-font-patcher \
+                  --mono \
+                  --adjust-line-height \
+                  --complete \
+                  --careful \
+                  --no-progressbars \
+                  --outputdir $out \
+                  $file
+              done
+
+              set +x
+            '';
+            dontInstall = true;
+          };
+
+          # web = pkgs.napalm.buildPackage nv.src {
+          #   pname = "${nv.pname}-web";
+          #   inherit (nv) version;
+          #   npmCommands = [
+          #     "npm install"
+          #     "npm run build --no-update-notifier -- webfont::iosevka-normal >/dev/null"
+          #   ];
+          #   nativeBuildInputs = [
+          #     pkgs.ttfautohint
+          #   ];
+          #   postPatch = ''
+          #     cp ${./private-build-plans.toml} private-build-plans.toml
+          #   '';
+          #   installPhase = ''
+          #     mkdir -p $out
+          #     find dist -type f -name '*.woff2' -exec cp -v '{}' $out \;
+          #     find dist -type f -name '*.css' -exec cp -v '{}' $out \;
+          #   '';
+          # };
+
+          zip-nerd =
+            pkgs.runCommand "iosevka-zip" {
+              src = config.packages.default;
               nativeBuildInputs = [
                 pkgs.zip
               ];
@@ -98,15 +129,28 @@
               cp -av "$WORKDIR/iosevka.zip" $out
             '';
 
-          nerd-fonts-src = pkgs.fetchFromGitHub {
-            owner = "ryanoasis";
-            repo = "nerd-fonts";
-            rev = "v2.1.0";
-            sparseCheckout = ''
-              src/glyphs
-            '';
-            hash = "sha256-Vhyd1jCsDNIVNE/WF2bxAcmRguEwj6i3OqBC1fxi1S4=";
-          };
+          # web-zipfile =
+          #   pkgs.runCommand "iosevka-web-zip" {
+          #     src = self'.packages.web;
+          #     nativeBuildInputs = [
+          #       pkgs.zip
+          #     ];
+          #   } ''
+          #     WORKDIR="$PWD"
+          #     cd $src
+          #     zip "$WORKDIR/iosevka.zip" *
+          #     cp -av "$WORKDIR/iosevka.zip" $out
+          #   '';
+
+          # nerd-fonts-src = pkgs.fetchFromGitHub {
+          #   owner = "ryanoasis";
+          #   repo = "nerd-fonts";
+          #   rev = "v2.1.0";
+          #   sparseCheckout = ''
+          #     src/glyphs
+          #   '';
+          #   hash = "sha256-Vhyd1jCsDNIVNE/WF2bxAcmRguEwj6i3OqBC1fxi1S4=";
+          # };
         };
       };
     };
