21b414ff7e22df35ad181a88fc022bac42610831
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-07-08 14:35:17 +0200

    tweak ci

--- ci.sh
+++ ci.sh
@@ -6,9 +6,10 @@ nix build .#iosevka-normal.base -L
 DIST="$(realpath "${1:-$PWD/dist}")"
 mkdir -p "$DIST"
 
-pushd ./result/TTF
-zip -9 -r "$DIST/iosevka-ttf.zip" ./.
-popd
+# no need for ttf-only package
+# pushd ./result/TTF
+# zip -9 -r "$DIST/iosevka-ttf.zip" ./.
+# popd
 
 pushd ./result
 zip -9 -r "$DIST/iosevka.zip" ./.
